1646008636aae73a606745caa2d16685da79fc61
TomyB <123563755+TomyBDev@users.noreply.github.com>
2024-02-02 21:22:50 +0000

    changes

---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+document.querySelector(".linkedinbutton").addEventListener("click", linkToLinkedIn);
+
+function linkToLinkedIn()
+{
+    window.location.href = "https://www.linkedin.com/in/thomas-ballantyne-1a5247267/";
+}
