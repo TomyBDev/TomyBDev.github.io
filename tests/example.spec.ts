2f4dfaba52158de582294e0fe4dda9b1ac6f9b4d
TomyB <123563755+TomyBDev@users.noreply.github.com>
2024-02-09 23:18:49 +0000

    test test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://thomasbgamedev.net');
+  await page.goto('http://localhost:4000');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/ThomasB/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://thomasbgamedev.net/cv');
+  await page.goto('http://localhost:4000/cv');
 
   // Click the get started link.
   //await page.getByRole('link', { name: 'CV' }).click();
