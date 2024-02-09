c5980f686b852fd23d2155f4d3d9981f4b667cb8
TomyB <123563755+TomyBDev@users.noreply.github.com>
2024-02-09 23:06:36 +0000

    Added Playright test unit cases

---
+++ tests/example.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://thomasbgamedev.net');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/ThomasB/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://thomasbgamedev.net/cv');
+
+  // Click the get started link.
+  //await page.getByRole('link', { name: 'CV' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page).toHaveTitle(/CV/);
+});
