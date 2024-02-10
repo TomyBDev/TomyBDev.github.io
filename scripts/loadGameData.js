c6dd43c1253cef7afc2651f41df9e5997e7363eb
TomyB <123563755+TomyBDev@users.noreply.github.com>
2024-02-10 03:36:40 +0000

    Videos play when game tiles are hovered.

--- scripts/loadGameData.js
+++ scripts/loadGameData.js
@@ -1,10 +1,33 @@
 fetch("../data/gridData.json")
   .then(response => response.json())
   .then(gameData => {
-for (var k in gameData) {
-    document.getElementById("game-block").innerHTML += '<div class="game-block-tile"><img class="game-block-img" src="' + gameData[k]['imageUrl'] + '" alt="' + gameData[k]['title'] + '"><h3 class="game-block-title">' + gameData[k]['title'] + '</h3><p class="game-block-info">' + gameData[k]['description'] + '</p></div>';
-};
-})
-.catch(error => {
-  console.error('Error fetching JSON:', error);
-});
+    var gameBlock = document.getElementById("game-block");
+
+    for (var k in gameData) {
+      var tileHtml = '<div class="game-block-tile"><video class="game-block-video"><source src="' + gameData[k]['videoUrl'] + '" type="video/mp4"></video><img class="game-block-img" src="' + gameData[k]['imageUrl'] + '" alt="' + gameData[k]['title'] + '"><h3 class="game-block-title">' + gameData[k]['title'] + '</h3><p class="game-block-info">' + gameData[k]['description'] + '</p></div>';
+      
+      gameBlock.innerHTML += tileHtml;
+    }
+
+    let gameBlockTiles = gameBlock.querySelectorAll('.game-block-tile');
+    gameBlockTiles.forEach(function(gameBlockTile) {
+      let video = gameBlockTile.querySelector('.game-block-video');
+
+      video.loop = true; // Set video to loop
+      video.muted = true;
+
+      gameBlockTile.addEventListener('mouseenter', function() {
+        video.play(); // Start playing the video
+        video.style.visibility = 'visible';
+      });
+      
+      gameBlockTile.addEventListener('mouseleave', function() {
+        video.pause(); // Pause the video
+        video.currentTime = 0;
+        video.style.visibility = 'hidden'; // Hide the video
+      });
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching JSON:', error);
+  });
