54120a45718eec1219f2bc26cbf1bf75f67360cc
TomyB <123563755+TomyBDev@users.noreply.github.com>
2024-02-10 01:26:45 +0000

    updated tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,12 +7,21 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/ThomasB/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('/cv');
+test('check social buttons', async ({ page }) => {
+  await page.goto('/');
 
-  // Click the get started link.
-  //await page.getByRole('link', { name: 'CV' }).click();
+  let context = await page.context();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page).toHaveTitle(/CV/);
+  // Start waiting for new page before clicking. Note no await.
+const pagePromise = context.waitForEvent('page');
+await page.getByRole('button', { name: 'LinkedIn' }).click();
+const newPage = await pagePromise;
+await newPage.waitForLoadState();
+await expect(newPage).toHaveURL(/www.linkedin.com/);
+});
+
+test('game tile data loading', async ({ page }) => {
+  await page.goto('/');
+
+  await expect(page.getByAltText('Evolution Cycle')).toBeVisible();
 });
