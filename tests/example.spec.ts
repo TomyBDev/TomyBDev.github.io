ca31eb108700f5f7c70753899452b4ee20a59998
TomyB <123563755+TomyBDev@users.noreply.github.com>
2024-02-10 15:36:53 +0000

    Updated test to potentially be less flaky by waiting for dom content

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,6 +3,10 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('/');
 
+  await page.waitForLoadState('domcontentloaded', { timeout: 15000 });
+  await page.waitForLoadState('load', { timeout: 30000 });
+  await page.waitForLoadState('networkidle', { timeout: 5000 });
+
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/ThomasB/);
 });
@@ -10,18 +14,26 @@ test('has title', async ({ page }) => {
 test('check social buttons', async ({ page }) => {
   await page.goto('/');
 
+  await page.waitForLoadState('domcontentloaded', { timeout: 15000 });
+  await page.waitForLoadState('load', { timeout: 30000 });
+  await page.waitForLoadState('networkidle', { timeout: 5000 });
+
   let context = await page.context();
 
-  // Start waiting for new page before clicking. Note no await.
-const pagePromise = context.waitForEvent('page');
-await page.getByRole('button', { name: 'LinkedIn' }).click();
-const newPage = await pagePromise;
-await newPage.waitForLoadState();
-await expect(newPage).toHaveURL(/www.linkedin.com/);
+    // Start waiting for new page before clicking. Note no await.
+  const pagePromise = context.waitForEvent('page');
+  await page.getByRole('button', { name: 'LinkedIn' }).click();
+  const newPage = await pagePromise;
+  await newPage.waitForLoadState();
+  await expect(newPage).toHaveURL(/www.linkedin.com/);
 });
 
 test('game tile data loading', async ({ page }) => {
   await page.goto('/');
+
+  await page.waitForLoadState('domcontentloaded', { timeout: 15000 });
+  await page.waitForLoadState('load', { timeout: 30000 });
+  await page.waitForLoadState('networkidle', { timeout: 5000 });
 
   await expect(page.getByAltText('Evolution Cycle')).toBeVisible();
 });
