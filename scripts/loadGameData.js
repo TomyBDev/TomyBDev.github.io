2dc1e8143688977f5e455e15922152399be75484
TomyB <123563755+TomyBDev@users.noreply.github.com>
2024-02-10 01:37:28 +0000

    fixing platform support due to assert in js

--- scripts/loadGameData.js
+++ scripts/loadGameData.js
@@ -1,5 +1,10 @@
-import gameData from "../data/gridData.json" assert { type: "json" };
-
+fetch("../data/gridData.json")
+  .then(response => response.json())
+  .then(gameData => {
 for (var k in gameData) {
     document.getElementById("game-block").innerHTML += '<div class="game-block-tile"><img class="game-block-img" src="' + gameData[k]['imageUrl'] + '" alt="' + gameData[k]['title'] + '"><h3 class="game-block-title">' + gameData[k]['title'] + '</h3><p class="game-block-info">' + gameData[k]['description'] + '</p></div>';
 };
+})
+.catch(error => {
+  console.error('Error fetching JSON:', error);
+});
