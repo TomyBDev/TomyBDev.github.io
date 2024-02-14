5b675aa51028f2f41b2d752f0e1b67f3e062deae
TomyB <123563755+TomyBDev@users.noreply.github.com>
2024-02-14 15:39:55 +0000

    Finished Evolution Cycle Page

---
+++ scripts/infoLinks.js
@@ -0,0 +1,12 @@
+document.getElementById("ec-exe-btn").addEventListener("click", linkToECEXE);
+document.getElementById("ec-source-btn").addEventListener("click", linkToECSource);
+
+function linkToECEXE()
+{
+    window.open("https://drive.google.com/file/d/1or9UE60MnG6gFky3jzfvCODusGNuIPuj/view?usp=sharing");
+}
+
+function linkToECSource()
+{
+    window.open("https://github.com/TomyBDev/AntPlusReader");
+}
