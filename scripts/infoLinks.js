18ed9ae8f02d80f3cc67918ff1045b69989c2daa
TomyB <123563755+TomyBDev@users.noreply.github.com>
2024-02-15 01:44:17 +0000

    Started populating other pages

--- scripts/infoLinks.js
+++ scripts/infoLinks.js
@@ -1,12 +1,98 @@
-document.getElementById("ec-exe-btn").addEventListener("click", linkToECEXE);
-document.getElementById("ec-source-btn").addEventListener("click", linkToECSource);
 
-function linkToECEXE()
+if (document.getElementById("tp-exe-btn") != null)
 {
-    window.open("https://drive.google.com/file/d/1or9UE60MnG6gFky3jzfvCODusGNuIPuj/view?usp=sharing");
+    document.getElementById("tp-exe-btn").addEventListener("click", function() {
+        window.open("https://drive.google.com/file/d/1eZHgiFQ85eBBZit7z1QE9prxPLIsCMjZ/view?usp=sharing");
+    });
 }
 
-function linkToECSource()
+if (document.getElementById("tp-source-btn") != null)
 {
-    window.open("https://github.com/TomyBDev/AntPlusReader");
+    document.getElementById("tp-source-btn").addEventListener("click", function() {
+        window.open("https://github.com/TomyBDev/Tiny-Planets");
+    });
+}
+
+if (document.getElementById("ec-exe-btn") != null)
+{
+    document.getElementById("ec-exe-btn").addEventListener("click", function() {
+        window.open("https://drive.google.com/file/d/1or9UE60MnG6gFky3jzfvCODusGNuIPuj/view?usp=sharing");
+    });
+}
+
+if (document.getElementById("ec-source-btn") != null)
+{
+    document.getElementById("ec-source-btn").addEventListener("click", function() {
+        window.open("https://github.com/TomyBDev/AntPlusReader");
+    });
+}
+
+if (document.getElementById("tg-exe-btn") != null)
+{
+    document.getElementById("tg-exe-btn").addEventListener("click", function() {
+        window.open("https://drive.google.com/file/d/18sanKSyz5Izg_O9eRqZMxd0sC390cp05/view?usp=sharing");
+  });
+}
+
+if (document.getElementById("tg-source-btn") != null)
+{
+    document.getElementById("tg-source-btn").addEventListener("click", function() {
+        window.open("https://github.com/TomyBDev/TankGame");
+    });
+}
+
+if (document.getElementById("vi-exe-btn") != null)
+{
+    document.getElementById("vi-exe-btn").addEventListener("click", function() {
+        window.open("https://drive.google.com/file/d/1FlymAIs_HkZq698KSzvHJatEZqBRm5UJ/view?usp=sharing");
+  });
+}
+
+if (document.getElementById("vi-source-btn") != null)
+{
+    document.getElementById("vi-source-btn").addEventListener("click", function() {
+        window.open("https://github.com/TomyBDev/PirateSoftwareGJ");
+    });
+}
+
+if (document.getElementById("hp-exe-btn") != null)
+{
+    document.getElementById("hp-exe-btn").addEventListener("click", function() {
+        window.open("https://drive.google.com/file/d/14EoYPnJJAaFX0eYl1kjZ2ANRSIMm0obV/view?usp=sharing");
+  });
+}
+
+if (document.getElementById("hp-source-btn") != null)
+{
+    document.getElementById("hp-source-btn").addEventListener("click", function() {
+        window.open("https://github.com/TomyBDev/Mizu");
+    });
+}
+
+if (document.getElementById("cc-exe-btn") != null)
+{
+    document.getElementById("cc-exe-btn").addEventListener("click", function() {
+        window.open("https://drive.google.com/file/d/1cMzJkhs2qaXEF0MkWqAtx9mQiEO4KNBX/view?usp=sharing");
+  });
+}
+
+if (document.getElementById("cc-source-btn") != null)
+{
+    document.getElementById("cc-source-btn").addEventListener("click", function() {
+        
+    });
+}
+
+if (document.getElementById("ti-exe-btn") != null)
+{
+    document.getElementById("ti-exe-btn").addEventListener("click", function() {
+        window.open("https://drive.google.com/file/d/1Fzc_8nR7hF6ch0-gf2VKrJoslZo2bMu9/view?usp=sharing");
+  });
+}
+
+if (document.getElementById("ti-source-btn") != null)
+{
+    document.getElementById("ti-source-btn").addEventListener("click", function() {
+        
+    });
 }
